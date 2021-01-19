e2ae518a12d967e72cc3162343990d207879b835
Haroun Jaradat <jj.haroon99@gmail.com>
2021-01-19 19:22:18 +0200

    deploy withU

--- apis/urls.py
+++ apis/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from  .views import ListTodo,DetailTodo
+from  .views import ListTodo,DetailTodo,getdata
 
 urlpatterns =[
     path('',ListTodo.as_view()),
     path('<int:pk>/',DetailTodo.as_view()),
+    path('one/<int:pk>/',getdata),
 ]
--- apis/views.py
+++ apis/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from  rest_framework import generics
+from  rest_framework.response import Response  
+from  rest_framework.decorators import api_view 
 from todos import models
 from .serializers import TodoSerializer
 
@@ -10,3 +12,9 @@ class ListTodo(generics.ListCreateAPIView):
 class DetailTodo(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Todo.objects.all()
     serializer_class = TodoSerializer
+@api_view(['GET'])
+def getdata(request,pk):
+    queryset = models.Todo.objects.get(id=pk)
+    serializer = TodoSerializer(queryset)
+    print(serializer.data)
+    return Response(serializer.data)
